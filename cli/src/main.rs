482f997e8d75cc3687bb5ba79e001b92c735bbc0
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2022-05-04 21:11:35 +0530

    cleaned up code

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,73 +2,8 @@ use std::io;
 use std::io::Write;
 use std::fs;
 
-
 fn main() {
-    // run_interpreter();
     generate_dynamic();
-}
-
-fn run_interpreter(){
-    let base_directory: String = String::from("C:\\portfolio\\tanishq");
-    let directories: Vec<&str> = vec!["education", "experiences", "social", "skills", "projects", "about"];
-    loop{
-        let mut input: String = String::new();
-        print!("{}>", base_directory);
-        io::stdout().flush().unwrap();
-        match io::stdin().read_line(&mut input){
-            Ok(_ok) => {
-                let command = input.trim();
-                if command == "help"{ display_help() }
-                else if command == "quit"{ break }
-                else if command.starts_with("cd"){ 
-                    let command_items: Vec<&str> = command.split(" ").collect();
-                    let location: &str = command_items[1];
-                    // or we can have a different loop systems for each type of command, enums can be used.
-                    match location{
-                        "education" => {
-                            // let result = execute_education(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        "experience" => {
-                            // let result = execute_experience(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        "social" => {
-                            // let result = execute_social(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        "skills" => {
-                            // let result = execute_skills(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        "projects" => {
-                            // let result = execute_projects(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        "about" => {
-                            // let result = execute_about(&base_directory);
-                            // if result{
-                            //     break
-                            // }
-                        },
-                        _ => println!("Invalid Command. Use 'help' for more information about commands.")
-                    }
-                }
-                else { println!("Invalid Command. Use 'help' for more information about commands.") }
-            },
-            Err(e) => println!("There was an error {}", e)
-        }
-    }
 }
 
 fn display_help(){
